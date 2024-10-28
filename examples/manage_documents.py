ed428ac361298a18ef98e115b34a77fd70e708d0
boredAMAM <163340308+boredAMAM@users.noreply.github.com>
2024-10-28 19:20:52 +0200

    improve variables

--- examples/manage_documents.py
+++ examples/manage_documents.py
@@ -1,11 +1,12 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()  # Loads environment variables from a .env file.
 
-def demo_env_variables():
-    example_api_key = os.getenv("EXAMPLE_API_KEY", "DefaultApiKey")
-    print(f"Using API Key: {example_api_key}")
+def display_environment_variable():
+    # Retrieves the API Key from environment variables; uses a default value if not found.
+    apiKey = os.getenv("EXAMPLE_API_KEY", "DefaultApiKey")
+    print(f"Using API Key: {apiKey}")
 
 if __name__ == "__main__":
-    demo_env_variables()
+    display_environment_variable()
