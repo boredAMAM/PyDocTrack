c47bae2e18e0df44f78862f897abab7a47ba29aa
boredAMAM <163340308+boredAMAM@users.noreply.github.com>
2024-12-23 19:50:49 +0200

    advance the readability

--- doc-track/init.py
+++ doc-track/init.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 
+# Load environment variables from a .env file
 load_dotenv()
 
+# Get the DATABASE_URL environment variable
 DATABASE_URL = os.getenv('DATABASE_URL')
